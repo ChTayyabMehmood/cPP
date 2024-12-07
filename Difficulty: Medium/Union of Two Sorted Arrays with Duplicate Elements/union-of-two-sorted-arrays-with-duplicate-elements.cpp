93cad92c269fe5e899683b5fc6327b04d6d80edd
ChTayyabMehmood <ChTayyabMehmod@Gmail.com>
2024-12-07 17:56:56 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of Two Sorted Arrays with Duplicate Elements/union-of-two-sorted-arrays-with-duplicate-elements.cpp
+++ Difficulty: Medium/Union of Two Sorted Arrays with Duplicate Elements/union-of-two-sorted-arrays-with-duplicate-elements.cpp
@@ -11,30 +11,36 @@ class Solution {
     // Function to return a list containing the union of the two arrays.
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         
-        int n=a.size();
-        int m=b.size();
+        int n=a.size(),m=b.size();
         
-        set <int> st;
-        
-        for(int i=0;i<n;i++)
-         st.insert(a[i]);
-         
-        for(int i=0;i<m;i++){
-            
-            st.insert(b[i]);
-        }
-        
-        vector<int> ans;
-        
-      for (auto it = st.begin(); it != st.end(); ++it) {
-            ans.push_back(*it);
-        }
-        
-        
-        return ans;
-        
-        
-        
+       int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (a[i] <= b[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != a[i])
+        Union.push_back(a[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != b[j])
+        Union.push_back(b[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != a[i])
+      Union.push_back(a[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != b[j])
+      Union.push_back(b[j]);
+    j++;
+  }
+  return Union;
         
     }
 };
