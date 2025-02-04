64bd7b09c9f0b6331170600ef17480faf08172c5
ChTayyabMehmood <ChTayyabMehmod@Gmail.com>
2025-02-04 22:01:30 +0500

    Time: 0 ms (100%), Space: 17.5 MB (53.04%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        
-        int low =0,high=nums.size()-1;
+       
+        int low=0,high=nums.size() - 1;
+        vector <int> ans ;
+        int first=-1,last=-1;
+        int mid;
+        while(low<=high){
+            
+            mid=(low+high)/2;
 
-         vector <int> res;
+            if(nums[mid]==target){
+                high=mid-1;
+                first = mid;
 
-        auto lb = lower_bound(nums.begin(), nums.end(), target); // First element >= key
-        auto ub = upper_bound(nums.begin(), nums.end(), target); // First element > key it return iterator 
-
-int start = lb-nums.begin(), end=ub-nums.begin()-1;
-
-
-
-        if(start==nums.size() || nums[start] != target){
-
-         res.push_back(-1);
-         res.push_back(-1);
-         return res;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else 
+                high=mid-1;
 
         }
 
-       
+        if(first ==-1){ ans.push_back(-1); ans.push_back(-1); return ans;}
 
-        res.push_back(start);
-        res.push_back(end );
+        low=0, high=nums.size()-1;
 
-        return res;
+        while(low<=high){
+
+            mid=(low+high)/2;
+
+            if(nums[mid]==target){
+                last = mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else 
+            high=mid-1;
+        }
+
+
+        
+        
+ ans.push_back(first);
+ ans.push_back(last);
+
+ return ans;
+
 
     }
 };
